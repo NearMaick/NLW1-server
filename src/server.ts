de1db89bef169dda0280114637dd2475947e301f
Maick Souza <maick_a_s@msn.com>
2020-10-11 11:22:25 -0300

    feat(server): setting routes

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(express.json());
 
-  response.json([
-    'Maick',
-    'Enilda',
-    'Tia Neuza',
-    'Tia Davi'
-  ]);
+const users = [
+  'Maick',
+  'Enilda',
+  'Tia Neuza',
+  'Tia Davi'
+];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
+})
 
 app.listen(3333);
